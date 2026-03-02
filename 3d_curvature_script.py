0eea92970d6a83bd33fd6948de2e42eb24045311
Firkenol <tabanlilatif@gmail.com>
2026-03-02 10:06:11 -0500

    Basic zoom slider added

--- 3d_curvature_script.py
+++ 3d_curvature_script.py
@@ -4,7 +4,7 @@ from matplotlib.widgets import Slider
 
 # 3d plot setup
 fig = plt.figure(figsize=(9, 9))
-plt.subplots_adjust(bottom=0.25)
+plt.subplots_adjust(bottom=0.3)
 fig.patch.set_facecolor('black')
 
 # native 3d projection
@@ -15,6 +15,7 @@ ax.set_facecolor('black')
 ax.axis('off')
 
 # init vars
+Zoom_init = 8.0
 A_init = 0.85
 N_init = 37.5
 B_init = 6.4
@@ -79,25 +80,31 @@ for i in range(4):
     line, = ax.plot(arms[i][0], arms[i][1], arms[i][2], color=colors[i], lw=1.5)
     lines.append(line)
 
-ax.set_xlim([-8, 8])
-ax.set_ylim([-8, 8])
-ax.set_zlim([-8, 8])
+ax.set_xlim([-Zoom_init, Zoom_init])
+ax.set_ylim([-Zoom_init, Zoom_init])
+ax.set_zlim([-Zoom_init, Zoom_init])
 ax.set_title('Native 3D Milky Way (Click and Drag to Rotate)', color='white')
 
 # slider ui
+ax_Zoom = plt.axes([0.15, 0.20, 0.7, 0.02], facecolor='darkgray')
 ax_N = plt.axes([0.15, 0.15, 0.7, 0.02], facecolor='darkgray')
 ax_B = plt.axes([0.15, 0.10, 0.7, 0.02], facecolor='darkgray')
 ax_T = plt.axes([0.15, 0.05, 0.7, 0.02], facecolor='darkgray')
 
+slider_Zoom = Slider(ax_Zoom, 'Zoom (Scale)', 2.0, 20.0, valinit=Zoom_init, color='#8d9cf5')
 slider_N = Slider(ax_N, 'Winding (N)', 5.0, 50.0, valinit=N_init, color='#c88df5')
 slider_B = Slider(ax_B, 'Bulge (B)', 0.1, 10.0, valinit=B_init, color='#c88df5')
 slider_T = Slider(ax_T, 'Rotation (T)', 0.0, 3.0, valinit=T_init, color='#c88df5')
 
-for s in [slider_N, slider_B, slider_T]:
+for s in [slider_Zoom, slider_N, slider_B, slider_T]:
     s.label.set_color('white')
     s.valtext.set_color('white')
 
 def update(val):
+    z_val = slider_Zoom.val
+    ax.set_xlim([-z_val, z_val])
+    ax.set_ylim([-z_val, z_val])
+    ax.set_zlim([-z_val, z_val])
     new_arms = calc_3d(A_init, slider_N.val, slider_B.val, C_init, Ct_init, slider_T.val)
     
     for idx, line in enumerate(lines):
@@ -109,5 +116,6 @@ def update(val):
 slider_N.on_changed(update)
 slider_B.on_changed(update)
 slider_T.on_changed(update)
+slider_Zoom.on_changed(update)
 
 plt.show()
